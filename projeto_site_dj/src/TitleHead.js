b6b13165bded740e2f47207c10d60c3cee17143e
ScarletBird <fgfogolin@hotmail.com>
2019-12-24 20:57:00 -0300

    Menu responsive for mobile

--- projeto_site_dj/src/TitleHead.js
+++ projeto_site_dj/src/TitleHead.js
@@ -5,35 +5,31 @@ class TitleHead extends React.Component {
     render() {
         return(
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark parentBar">
-                <div className="navbar navbar-dark bg-dark navbar-expand-lg MainNavbar">
-                    <div className="navbar title"><a href="#home" className="nav-item nav-link logo-title">ËSGÖTö</a></div>
-                    <div className="navbar logo">
-                        <a href="#home" className="navbar-item logo-title">
-                        <img src={esgotoLogo} className="logo" alt="Esgoto" width="70px" height="70px"/>
-                        </a> 
-                    </div>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav mr-auto">
-                        <li className="nav-item dropdown">
-                            <a href="#home" className="nav-item nav-link">HOME</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a href="#home" className="nav-item nav-link">ÁLBUNS</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a href="#home" className="nav-item nav-link">PODCASTS</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a href="#home" className="nav-item nav-link">ARTISTAS</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a href="#home" className="nav-item nav-link">CONTATO</a>
-                        </li>
-                        </ul>
-                    </div>
+                <a href="#home" className="navbar-brand logo-title">ËSGÖTö</a>
+                <a href="#home" className="navbar-item logo-title">
+                    <img src={esgotoLogo} className="logo" alt="Esgoto" width="70px" height="70px"/>
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item dropdown">
+                        <a href="#home" className="nav-item nav-link">HOME</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a href="#home" className="nav-item nav-link">ÁLBUNS</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a href="#home" className="nav-item nav-link">PODCASTS</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a href="#home" className="nav-item nav-link">ARTISTAS</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a href="#home" className="nav-item nav-link">CONTATO</a>
+                    </li>
+                    </ul>
                 </div>
             </nav>
         )
